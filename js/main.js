483a98936fe4b802e26470c5ba54c6e7accd3ef2
Therakar <matteo.pau200@gmail.com>
2022-10-26 16:45:45 +0200

    problema inserimento elemnto nel DOM

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 "use strict"
 
 // array che contiene le immagini
-
 const images = [`img/01`, `img/02`, `img/03`, `img/04`, `img/05`,];
+
+
+
+// ciclo for che va a ciclare l'array images
+for (let i = 0; i < images.length; i++){
+    const element = document.createElement(`div`);
+    element.innerHTML =`<div class="item"><img src="${images[i]}" alt="img"></div>`; 
+
+    document.getElementById("carousel-container").appendChild(`<div class="item"><img src="${images[i]}" alt="img"></div>`);
+}
