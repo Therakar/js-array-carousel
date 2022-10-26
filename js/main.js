bde92d5a243be91c1fc15de6845bc682d51e01bc
Therakar <matteo.pau200@gmail.com>
2022-10-26 17:00:12 +0200

    crea i div con la classe item nel js e li aggiunge al'HTML

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 "use strict"
 
 // array che contiene le immagini
-const images = [`img/01`, `img/02`, `img/03`, `img/04`, `img/05`,];
+const images = [`img/01.jpg`, `img/02.jpg`, `img/03.jpg`, `img/04.jpg`, `img/05.jpg`,];
 
 
 
 // ciclo for che va a ciclare l'array images
 for (let i = 0; i < images.length; i++){
     const element = document.createElement(`div`);
-    element.innerHTML =`<div class="item"><img src="${images[i]}" alt="img"></div>`; 
-
-    document.getElementById("carousel-container").appendChild(`<div class="item"><img src="${images[i]}" alt="img"></div>`);
+    element.innerHTML =`<img src="${images[i]}" alt="img">`; 
+    element.classList.add("item")
+    document.getElementById("carousel-container").append(element);
 }
